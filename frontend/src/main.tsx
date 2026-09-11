e3a9655141ab487ddb41d5937a8288c9c6978e52
Patrick <phat.ho.93@gmail.com>
2026-09-11 18:19:19 +1000

    Frontend: implement authentication components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Amplify } from 'aws-amplify'
+import '@aws-amplify/ui-react/styles.css'
 import './index.css'
 import App from './App.tsx'
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.VITE_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
